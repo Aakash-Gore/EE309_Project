96466ed474ef06d7d472d83ed20029b01a84eca4
Vinay <97208542+VStr20@users.noreply.github.com>
2023-05-03 16:58:03 +0530

    ALU2.vhd

--- ALU2.vhd
+++ ALU2.vhd
@@ -6,6 +6,9 @@ entity ALU2  is
   port(	
   
          sel : in std_logic;
+	                carry_encoding: in std_logic;
+	                zero_encoding: in std_logic;
+	                ext_carry: in std_logic_vector(15 downto 0);
 			A: in std_logic_vector(15 downto 0); 
 			B: in std_logic_vector(15 downto 0);
 			c_flag: out std_logic;
@@ -40,48 +43,78 @@ architecture Struct of ALU2 is
 					  
 	return sum;
 	end add;
-
-							
-				
+			
 	begin
-	   
-		alu : process( A, B)
-		  variable temp1 : std_logic_vector(16 downto 0);
-		  variable temp2 : std_logic_vector(16 downto 0);
-		  
-		  begin
-		   
-			case sel is
-			   when '0' =>   --addition
-				
-			      temp1 := add(A,B);
-					C <= temp1(15 downto 0);
-					c_flag <= temp1(16);
-					
-					if temp1(15 downto 0) = "0000000000000000" then
-					    z_flag <= '1';
-					else
-					    z_flag <= '0';
-					 end if;
-				
-						 
-				 when '1' =>  --nand
-				  
-				   l3 : for i in 0 to 15 loop
-                         temp1(i) := A(i) nand B(i);
-				    end loop l3;
-	  
-                	  C <= temp1(15 downto 0);
-						  if temp1(15 downto 0) = "0000000000000000" then
-					        z_flag <= '1';
-					     else
-					        z_flag <= '0';
-							end if;
-				 
-				   
-				 when others =>
-				    C <= "0000000000000000";
-					 
+		
+		     alu : process( A, B)
+			  variable D: std_logic_vector(15 downto 0);
+			  variable temp1 : std_logic_vector(16 downto 0);
+			  variable temp2 : std_logic_vector(16 downto 0);
+
+			  begin
+			     if carry_encoding = '1' and zero_encoding = '1' then
+
+				case sel is
+				   when '0' =>   --addition
+
+				      temp1 := add(A,B);
+						D <= temp1(15 downto 0);
+                                      temp2 := add(D,ext_carry);
+						c_flag <= temp1(16);
+
+						if temp2(15 downto 0) = "0000000000000000" then
+						    z_flag <= '1';
+						else
+						    z_flag <= '0';
+						 end if;
+				   when '1' =>  --nand
+
+					   l3 : for i in 0 to 15 loop
+				 temp1(i) := A(i) nand B(i);
+					    end loop l3;
+
+				  C <= temp1(15 downto 0);
+							  if temp1(15 downto 0) = "0000000000000000" then
+							z_flag <= '1';
+						     else
+							z_flag <= '0';
+								end if;
+
+
+					 when others =>
+					    C <= "0000000000000000";
+			     else 
+				 case sel is
+				   when '0' =>   --addition
+
+				      temp1 := add(A,B);
+						D <= temp1(15 downto 0);
+                                      temp2 := add(D,ext_carry);
+						c_flag <= temp1(16);
+
+						if temp2(15 downto 0) = "0000000000000000" then
+						    z_flag <= '1';
+						else
+						    z_flag <= '0';
+						 end if;
+				   when '1' =>  --nand
+
+					   l3 : for i in 0 to 15 loop
+				 temp1(i) := A(i) nand B(i);
+					    end loop l3;
+
+				  C <= temp1(15 downto 0);
+							  if temp1(15 downto 0) = "0000000000000000" then
+							z_flag <= '1';
+						     else
+							z_flag <= '0';
+								end if;
+
+
+					 when others =>
+					    C <= "0000000000000000";
+                              end if;
+
 			 end case;
 			
 		   end process;
