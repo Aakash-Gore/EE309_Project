76ba029a53b86369833cfa44705e0c62f4e6c60c
Vinay <97208542+VStr20@users.noreply.github.com>
2023-05-08 15:38:27 +0530

    Update demux.vhd

--- demux.vhd
+++ demux.vhd
@@ -4,15 +4,23 @@ use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity demux is
-    Port ( I,S : in  STD_LOGIC;
-           O1, O2 : out  STD_LOGIC);
-end demux;
+    Port ( I : in std_logic_vector(15 downto 0);
+	        S : in std_logic;
+           O1, O2 : out std_logic_vector(15 downto 0));
+end entity;
 
-architecture bhv of DEMUX_SOURCE is
+architecture bhv of demux is
 
 begin
+process(I, S)
+begin
+if S <= '0' then
+   O1 <= I;
+	O2 <= "0000000000000000";
+elsif S <= '1' then
+   O2 <= I;
+	O1 <= "0000000000000000";
+end if;
 
-O1 <= I and (not S);
-O2 <= I and S;
-
+end process;
 end bhv;
