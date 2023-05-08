d2ac9f9bb940f60a5fd675d360a1eb958e7f897f
Vinay <97208542+VStr20@users.noreply.github.com>
2023-05-08 15:39:13 +0530

    Update Mux_4to1.vhd

--- Mux_4to1.vhd
+++ Mux_4to1.vhd
@@ -3,24 +3,27 @@ use IEEE.STD_LOGIC_1164.all;
  
 entity mux_4to1 is
  port(
-     A : in STD_LOGIC_VECTOR(3 downto 0);
+     A : in STD_LOGIC_VECTOR(15 downto 0);
+	  B : in STD_LOGIC_VECTOR(15 downto 0);
+	  C : in STD_LOGIC_VECTOR(15 downto 0);
+	  D : in STD_LOGIC_VECTOR(15 downto 0);
      S0,S1: in STD_LOGIC;
-     Z: out STD_LOGIC
+     Z: out STD_LOGIC_VECTOR(15 downto 0)
   );
 end mux_4to1;
  
 architecture bhv of mux_4to1 is
 begin
-process (A,S0,S1) is
+process (A,B,C,D,S0,S1) is
 begin
   if (S0 ='0' and S1 = '0') then
-      Z <= A(3);
+      Z <= D;
   elsif (S0 ='1' and S1 = '0') then
-      Z <= A(2);
+      Z <= C;
   elsif (S0 ='0' and S1 = '1') then
-      Z <= A(1);
+      Z <= B;
   else
-      Z <= A(0);
+      Z <= A;
   end if;
  
 end process;
