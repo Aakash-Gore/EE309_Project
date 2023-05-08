530f389df1c8075bf67efac8e25d7641d095d66a
Vinay <97208542+VStr20@users.noreply.github.com>
2023-05-08 15:35:41 +0530

    Update ALU3.vhd

--- ALU3.vhd
+++ ALU3.vhd
@@ -1,24 +1,29 @@
 library ieee;
 use ieee.std_logic_1164.all;
-
+use ieee.numeric_std.all;
 entity ALU3  is
 
   port(	
 			A: in std_logic_vector(15 downto 0); 
-			B: in std_logic_vector(15 downto 0);
+			B: buffer std_logic_vector(15 downto 0);
 			C: out std_logic_vector(15 downto 0)
 		);
 		
 end entity ALU3;
 
 architecture Struct of ALU3 is
-signal temp : std_logic_vector(15 downto 0);
-  begin
-     process(A, B)
-       temp <= B;
-       B <= std_logic_vector(signed(temp)*2);
+signal temp: std_logic_vector(15 downto 0);
+signal temp_main: std_logic_vector(31 downto 0);
+shared variable sum,carry : std_logic_vector(15 downto 0);
+shared variable two: std_logic_vector(15 downto 0):="1111111111111110";
+  begin  
+  process(A, B)
+     begin 
+	  
+		temp <= B;
+       temp_main <= std_logic_vector(signed(temp)*signed(two));
+		 B<=temp_main(15 downto 0);
 
-				begin
 				 l1:  for i in 0 to 15 loop
 							if i=0 then
 								sum(i) := A(i) Xor B(i) Xor '0';
@@ -29,7 +34,6 @@ signal temp : std_logic_vector(15 downto 0);
 							end if;
 				end loop l1;
 
-end add;
 						
 end process;
 		
